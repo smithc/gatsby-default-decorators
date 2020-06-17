fb92bb83357f24bed973b8658a52e730e60ffeda
Chris Smith <smithc.wpi@gmail.com>
2020-06-17 12:40:59 -0700

    Demo of an error handling decorator for both ErrorBoundary and useEffect scenarios

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,46 @@
 // gatsby-browser.js
-import * as React from "___react-original___"
 import { registerDecorator } from "webpack-decorators"
 import { DecoratorComponent } from "./src/decorators/Decorator"
 
 export const onClientEntry = () => {
   const decorator = {
     createElement: function (originalFunc, ...args) {
-      if (args[0] === "div" && args[2] && args[2] === "This is a text") {
-        return React.createElement(
+      const elementFactory = (type, config, children) => originalFunc(type, config, children);
+
+      const type = args[0];
+      const isFunc = typeof type === 'function';
+      const isProvider = isFunc && type.name === 'Provider';
+
+      if (isProvider || (args[0] === "div" && args[2] && args[2] === "This is a text")) {
+        const element = elementFactory(
           DecoratorComponent,
           { title: "cooperman" },
-          originalFunc(...args) //text.jsx
-        )
+          elementFactory(...args)
+        );
+
+        return element;
       }
+
       return originalFunc(...args)
     },
+
+    useEffect: function (originalFunc, ...args) {
+      const [ effect, deps ] = args;
+      const callback = () => {
+        const asyncWrapper = async () => {
+          try {
+            await effect();
+          } catch(ex) {
+            console.error(ex);
+          }
+        };
+        
+        asyncWrapper();
+      };
+
+      return originalFunc(callback, deps);
+    }
   }
 
-  registerDecorator("react", decorator, "createElement")
+  registerDecorator("react", decorator, "createElement", "useEffect");
 }
